048d8cc18811fbe82b1a942da3d82e5a01db9d9f
Alan Aliaga <alanaliagadev@gmail.com>
2018-10-16 15:22:00 -0400

    [Add] Pages (Footer)

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from 'components/shared/Header';
+import Footer from 'components/shared/Footer';
 
 import Home from './Home';
 
@@ -8,6 +9,7 @@ const Pages = () => (
   <div>
     <Header />
     <Home />
+    <Footer />
   </div>
 );
 
---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+  padding: 1rem 2rem;
+`;
+
+const List = styled.ul`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const Item = styled.li`
+  margin: 0.5rem;
+`;
+
+const Link = styled.a`
+  color: ${({ theme }) => theme.colors.darkgray};
+  text-decoration: none;
+`;
+
+const Footer = () => (
+  <Wrapper>
+    <List>
+      <Item>
+        <Link href="/">Busquedas populares</Link>
+      </Item>
+      <Item>
+        <Link href="/">Directorio</Link>
+      </Item>
+      <Item>
+        <Link href="/">Quienes somos</Link>
+      </Item>
+      <Item>
+        <Link href="/">Blog</Link>
+      </Item>
+      <Item>
+        <Link href="/">Trabajo</Link>
+      </Item>
+      <Item>
+        <Link href="/">Desarrolladores</Link>
+      </Item>
+      <Item>
+        <Link href="/">Ayuda</Link>
+      </Item>
+      <Item>
+        <Link href="/">Legal</Link>
+      </Item>
+      <Item>
+        <Link href="/">Privacidad</Link>
+      </Item>
+      <Item>
+        <Link href="/">Cookies</Link>
+      </Item>
+      <Item>
+        <Link href="/">SoundCloud Ltd.</Link>
+      </Item>
+    </List>
+  </Wrapper>
+);
+
+export default Footer;
