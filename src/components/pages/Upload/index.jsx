13e32187b3a2ae758b5220d15c2eb612d686f24b
allen4dev <alanaliagadev@gmail.com>
2018-11-26 00:54:56 -0400

    [ToDo] upload a new track

--- src/components/pages/Upload/index.jsx
+++ src/components/pages/Upload/index.jsx
@@ -1,8 +1,154 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import { Title, Text } from 'components/utils/Texts';
 import { Button } from 'components/utils/Buttons';
+
+const apiTags = [{ id: '1', name: 'JPop' }, { id: '2', name: 'Kpop' }];
+
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      tags: [],
+    };
+
+    this.photo = React.createRef();
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState(() => ({ [name]: value }));
+  };
+
+  handleTagChange = name => {
+    const { tags } = this.state;
+
+    let newTags = [...tags];
+
+    const tagName = name.toLowerCase();
+
+    if (tags.find(tag => tag === tagName)) {
+      newTags = tags.filter(tag => tag !== tagName);
+    } else {
+      newTags = tags.concat(tagName);
+    }
+
+    this.setState({ tags: newTags });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.photo);
+
+    const { title, tags } = this.state;
+    console.log(this.photo.current.files[0].name);
+    console.log(title);
+    console.log(tags);
+  };
+
+  render() {
+    const { title } = this.state;
+
+    return (
+      <Wrapper>
+        <Content onSubmit={this.handleSubmit}>
+          <Heading>
+            <Title color="dark" size="2rem">
+              Crea una nueva pista
+            </Title>
+          </Heading>
+
+          <Body>
+            <FormSection>
+              <Subtitle>Titulo de la pista</Subtitle>
+
+              <FormGroup>
+                <Input
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+            </FormSection>
+
+            <FormSection>
+              <Subtitle color="dark">Tags</Subtitle>
+
+              <CheckboxGroup>
+                {apiTags.map(tag => (
+                  <FormGroup key={tag.id}>
+                    <Input
+                      type="checkbox"
+                      name="tags[]"
+                      onChange={() => this.handleTagChange(tag.name)}
+                    />
+                    <Label>{tag.name.toLowerCase()}</Label>
+                  </FormGroup>
+                ))}
+              </CheckboxGroup>
+            </FormSection>
+
+            <FormSection>
+              <Subtitle>Imagen de la pista</Subtitle>
+
+              <FormGroup>
+                <input type="file" ref={this.photo} />
+              </FormGroup>
+            </FormSection>
+
+            <FormSection>
+              {/* <Button flexible bgColor="orange" color="white">
+            selecciona los archivos que quieras subir
+          </Button> */}
+              <Subtitle>Archivo de sonido de la pista</Subtitle>
+
+              <FormGroup>
+                <Input type="file" />
+              </FormGroup>
+            </FormSection>
+
+            <FormSection>
+              <FormGroup>
+                <Input type="checkbox" />
+                <Label>
+                  Crear una lista cuando hay varios archivos seleccionados
+                </Label>
+              </FormGroup>
+            </FormSection>
+          </Body>
+
+          <Footer>
+            <Sharing>
+              <Text>Disponibilidad</Text>
+
+              <RadioForm>
+                <Input type="radio" />
+                <Label>Publico</Label>
+              </RadioForm>
+
+              <RadioForm>
+                <Input type="radio" />
+                <Label>Privada</Label>
+              </RadioForm>
+            </Sharing>
+
+            <FormSection>
+              <Button type="submit" color="white" bgColor="orange">
+                Subir pista
+              </Button>
+            </FormSection>
+          </Footer>
+        </Content>
+      </Wrapper>
+    );
+  }
+}
 
 const Wrapper = styled.section`
   height: ${({ theme: { sizes } }) => `calc(100vh - ${sizes.total})`};
@@ -62,72 +208,5 @@ const Sharing = styled.div`
 `;
 
 const Footer = styled.footer``;
-
-const Upload = () => (
-  <Wrapper>
-    <Content>
-      <Heading>
-        <Title color="dark" size="2rem">
-          Crea una nueva pista
-        </Title>
-      </Heading>
-
-      <Body>
-        <FormSection>
-          <Subtitle>Titulo de la pista</Subtitle>
-          <FormGroup>
-            <Input type="text" />
-          </FormGroup>
-        </FormSection>
-
-        <FormSection>
-          <Subtitle color="dark">Tags</Subtitle>
-          <CheckboxGroup>
-            <FormGroup>
-              <Input type="checkbox" name="tags[]" />
-              <Label>J-Pop</Label>
-            </FormGroup>
-
-            <FormGroup>
-              <Input type="checkbox" name="tags[]" />
-              <Label>K-Pop</Label>
-            </FormGroup>
-          </CheckboxGroup>
-        </FormSection>
-
-        <FormSection>
-          <Button flexible bgColor="orange" color="white">
-            selecciona los archivos que quieras subir
-          </Button>
-        </FormSection>
-
-        <FormSection>
-          <FormGroup>
-            <Input type="checkbox" />
-            <Label>
-              Crear una lista cuando hay varios archivos seleccionados
-            </Label>
-          </FormGroup>
-        </FormSection>
-      </Body>
-
-      <Footer>
-        <Sharing>
-          <Text>Disponibilidad</Text>
-
-          <RadioForm>
-            <Input type="radio" />
-            <Label>Publico</Label>
-          </RadioForm>
-
-          <RadioForm>
-            <Input type="radio" />
-            <Label>Privada</Label>
-          </RadioForm>
-        </Sharing>
-      </Footer>
-    </Content>
-  </Wrapper>
-);
 
 export default Upload;
--- src/middlewares/api/index.js
+++ src/middlewares/api/index.js
@@ -3,13 +3,15 @@ import client from 'helpers/client';
 import * as actionTypes from './actionTypes';
 
 // eslint-disable-next-line
-const apiMiddleware = ({ dispatch }) => next => async action => {
+const apiMiddleware = ({ getState, dispatch }) => next => async action => {
   if (action.type !== actionTypes.API_REQUEST) return next(action);
 
   const { success } = action.payload;
   const { clientMethod, details = {} } = action.meta;
 
-  const response = await client[clientMethod](details);
+  const token = getState().auth.token || null;
+
+  const response = await client[clientMethod](details, token);
 
   dispatch(success(response));
 };
